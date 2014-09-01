7a125feab1de0bbc73de367ed54dba3dbbd6316f
teotigraphix <mschmalle@teotigraphix.com>
2014-09-01 07:03:10 -0400

    Restarting with framework 0.1.5 - Still trying to figure out pad lighting issue on my system.

--- MaschineStudio.control.js
+++ MaschineStudio.control.js
@@ -1,15 +1,8 @@
 // Written by Michael Schmalle - teotigraphix.com
 // (c) 2014
-// Licensed under GPLv3 - http://www.gnu.org/licenses/gpl.html
+// Licensed under LGPLv3 - http://www.gnu.org/licenses/lgpl-3.0.txt
 
 loadAPI(1);
-
-load("Config.js");
-load ("bitwig-framework/ClassLoader.js");
-load ("mode/ClassLoader.js");
-load ("view/ClassLoader.js");
-load ("core/ClassLoader.js");
-load ("Controller.js");
 
 host.defineController("Native Instruments", "Maschine Studio", "0.1", "342ED090-C483-11E3-9C1A-0800200C9A66");
 host.defineMidiPorts(1, 1);
@@ -17,21 +10,15 @@ host.addDeviceNameBasedDiscoveryPair(["Maschine Studio Controller"], ["Maschine 
 host.addDeviceNameBasedDiscoveryPair(["Maschine Studio Virtual Input"], ["Maschine Studio Virtual Output"]);
 host.addDeviceNameBasedDiscoveryPair(["Maschine Studio In"], ["Maschine Studio Out"]);
 
-var controller = null;
-
 function init ()
 {
-    controller = new Controller ();
-    controller.init ();
     println ("Initialized.");
 }
 
 function exit ()
 {
-    controller.shutdown ();
 }
 
 function flush ()
 {
-    controller.flush ();
 }
--- core/ClassLoader.js
+++
@@ -1,8 +0,0 @@
-// Written by Michael Schmalle - teotigraphix.com
-// (c) 2014
-// Licensed under GPLv3 - http://www.gnu.org/licenses/gpl.html
-
-load ("MaschineMidiInput.js");
-load ("MaschineButton.js");
-load ("MaschineStudio.js");
-load ("Grid.js");
